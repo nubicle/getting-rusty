cca3def52ad29da2e6674a1a24da352d75800c94
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-06-18 13:31:49 +0530

    I __ a word
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- custom-deserializer/src/lib.rs
+++ custom-deserializer/src/lib.rs
@@ -1,5 +1,52 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::{de::Visitor, Deserialize};
+
+#[derive(Default)]
+pub struct Person {
+    pub name: String,
+    pub age: u8,
+}
+
+impl<'de> Deserialize<'de> for Person {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(PersonVisitor {})
+    }
+}
+
+struct PersonVisitor;
+
+impl<'de> Visitor<'de> for PersonVisitor {
+    type Value = Person;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("Could not deserialize person")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        let mut name = None;
+        let mut age = None;
+
+        for pair in v.split('|') {
+            let mut parts = pair.splitn(2, ':');
+            match (parts.next(), parts.next()) {
+                (Some("Name"), Some(v)) => name = Some(v.to_string()),
+                (Some("Age"), Some(v)) => {
+                    age = v.parse::<u8>().ok();
+                }
+                _ => return Err(E::custom("Invalid key or value")),
+            }
+        }
+
+        let name = name.ok_or_else(|| E::missing_field("Name"))?;
+        let age = age.ok_or_else(|| E::missing_field("Age"))?;
+
+        Ok(Person { name, age })
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +55,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let data = "\"Name:John Doe|Age:30\"";
+        let p: Person = serde_json::from_str(data).expect("failed to deserialize");
+
+        assert_eq!(p.age, 30);
+        assert_eq!(p.name, String::from("John Doe"));
     }
 }
